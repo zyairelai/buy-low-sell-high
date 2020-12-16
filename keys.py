0717e841d5f517cd288c1d6780abfbc61ade79a8
louiszhenyean <louiszhenyean@gmail.com>
2020-12-17 00:09:24 +0800

    done keys

--- keys.py
+++ keys.py
@@ -13,9 +13,9 @@ api_owner   = os.environ.get('API_OWNER')
 api_key     = os.environ.get('API_KEY')
 api_secret  = os.environ.get('API_SECRET')
 
-# Validate Your Environment
-# print("API OWNER        :   " + api_owner)
-# print("API Key          :   " + api_key)
-# print("API Secret Key   :   " + api_secret)
+# Check Your Environment
+print("API OWNER        :   " + api_owner)
+print("API Key          :   " + api_key)
+print("API Secret Key   :   " + api_secret)
 
 # client = Client(keys.api_key, keys.api_secret)
