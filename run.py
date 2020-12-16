d5b0077d2677387c6f2ba282eec267a14518a65e
louiszhenyean <louiszhenyean@gmail.com>
2020-12-16 19:14:41 +0800

    add keys

--- run.py
+++ run.py
@@ -1,25 +1,22 @@
-import os
+import keys
 import sys
 import time
 from binance.client import Client
 
-# Get environment variables
-api_key = os.getenv('API_KEY')
-api_secret = os.environ.get('API_SECRET')
-client = Client(api_key, api_secret)
-
 # start = time.time()
 # print(f"{time.time() - start} seconds\n")
 
+client = Client(keys.api_key, keys.api_secret)
+symbol = "ETHUSDT"
+amount = "0.02"
+
 # Get Average Price Per 5 minute
-avg_price = client.get_avg_price(symbol='ETHUSDT')
+avg_price = client.get_avg_price(symbol=symbol)
 current = avg_price
-
-
 
 # place a test market buy order, to place an actual order use the create_order function
 order = client.create_test_order(
-    symbol      =   'ETHUSDT',
+    symbol      =   symbol,
     side        =   Client.SIDE_BUY,
     type        =   Client.ORDER_TYPE_MARKET,
-    quantity    =   100)
+    quantity    =   amount)
--- test.py
+++ test.py
@@ -1,15 +1,12 @@
-import os
+import datetime
+import keys
 import sys
 import time
 from binance.client import Client
 
-# Get environment variables
-api_owner = os.getenv('API_OWNER')
-api_key = os.getenv('API_KEY')
-api_secret = os.environ.get('API_SECRET')
-client = Client(api_key, api_secret)
+client = Client(keys.api_key, keys.api_secret)
+symbol = "ETHUSDT"
+amount = "0.02"
 
-
-# Test your algorithm here
-print(client.get_account())
-print(client.get_account().values(1))
+candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_30MINUTE)
+print(candles)
