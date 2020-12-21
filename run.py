e2c634936a67fa94e7ce2ade3843db1896a3738c
louiszhenyean <louiszhenyean@gmail.com>
2020-12-21 12:19:37 +0800

    DONE

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ import time
 import datetime
 from binance.client import Client
 
-symbol  = "ETHUSDT"
+asset   = "ETH"
+base    = "USDT"
+symbol  =  asset + base
 core    =  500
 
 # Get environment variables
@@ -15,7 +17,7 @@ while True:
     price_response = client.get_symbol_ticker(symbol=symbol)
     price = float(list(list(price_response.items())[1])[1])
 
-    balance_response = client.get_asset_balance(asset='ETH')
+    balance_response = client.get_asset_balance(asset=asset)
     balance = float(list(list(balance_response.items())[1])[1])
 
     current_core    = round((balance * price), 4)
@@ -24,30 +26,30 @@ while True:
 
     print(price_response)
     print("Created at           : " + str(datetime.datetime.now()))
-    print("Prefix Core          : " + str(core) + " USDT")
-    print("Current Core         : " + str(current_core) + " USDT")
+    print("Prefix Core  (" + asset + ")   : " + str(core) + " " + base)
+    print("Current Core (" + asset + ")   : " + str(current_core) + " " + base)
     print("Percentage Changed   : " + str(change_percent) + " %")
 
     if (current_core > core) and (abs(change_percent) >= 3.5):
         sell  = client.order_market_sell(symbol=symbol, quantity=trade_amount)
-        print("Action               : SELL " + str(trade_amount) + " ETH\n")
+        print("Action               : SELL " + str(trade_amount) + " " + asset + "\n")
         with open("logs.txt", "a") as trade_logs:
             trade_logs.write(str(price_response) + "\n")
             trade_logs.write("Created at            : " + str(datetime.datetime.now()) + "\n")
-            trade_logs.write("Prefix Core           : " + str(core) + " USDT\n")
-            trade_logs.write("Current Core          : " + str(current_core) + " USDT\n")
-            trade_logs.write("Percentage Changed    : " + str(change_percent) + " %\n")
-            trade_logs.write("Action                : SELL " + str(trade_amount) + " ETH\n\n")
+            trade_logs.write("Prefix Core  (" + asset + ")    : " + str(core) + " " + base + " \n")
+            trade_logs.write("Current Core (" + asset + ")    : " + str(current_core) + " " + base + " \n")
+            trade_logs.write("Percentage Changed    : " + str(change_percent) + " " + base + " \n")
+            trade_logs.write("Action                : SELL " + str(trade_amount) + " " + asset + "\n\n")
     elif (current_core < core) and (abs(change_percent) >= 3.5):
         buy   = client.order_market_buy(symbol=symbol, quantity=trade_amount)
         print("Action               : BUY " + str(trade_amount) + " ETH\n")
         with open("logs.txt", "a") as trade_logs:
             trade_logs.write(str(price_response) + "\n")
             trade_logs.write("Created at            : " + str(datetime.datetime.now()) + "\n")
-            trade_logs.write("Prefix Core           : " + str(core) + " USDT\n")
-            trade_logs.write("Current Core          : " + str(current_core) + " USDT\n")
+            trade_logs.write("Prefix Core  (" + asset + ")    : " + str(core) + " " + base + " \n")
+            trade_logs.write("Current Core (" + asset + ")    : " + str(current_core) + " " + base + " \n")
             trade_logs.write("Percentage Changed    : " + str(change_percent) + " %\n")
-            trade_logs.write("Action                : BUY " + str(trade_amount) + " ETH\n\n")
+            trade_logs.write("Action                : BUY " + str(trade_amount) + " " + asset + "\n\n")
     else:
         print("Action               : Do Nothing\n")
 
