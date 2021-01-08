37f180b659433f9333188631c38b7a38734f5000
louiszhenyean <louiszhenyean@gmail.com>
2021-01-08 11:10:43 +0800

    prettier

--- keys.py
+++ keys.py
@@ -6,9 +6,9 @@ from binance.client import Client
 # export API_KEY="binance_api_key"
 # export API_SECRET="binance_secret_key"
 
-# Get environment variables	
-api_owner   = os.environ.get('API_OWNER')	
-api_key     = os.environ.get('API_KEY')	
+# Get environment variables
+api_owner   = os.environ.get('API_OWNER')
+api_key     = os.environ.get('API_KEY')
 api_secret  = os.environ.get('API_SECRET')
 
 # Check Your Environment
--- test/run.py
+++ test/run.py
@@ -10,7 +10,7 @@ def buy_low_sell_high():
     core    =  500
 
     # Get environment variables
-    api_key     = os.environ.get('API_KEY')	
+    api_key     = os.environ.get('API_KEY')
     api_secret  = os.environ.get('API_SECRET')
     client      = Client(api_key, api_secret)
 
