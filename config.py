7f6b4520f50e4c4b585eed71ccb763927f3fc31c
Zyaire <zyairelai@gmail.com>
2021-02-18 21:47:46 +0800

    add btc pairs

--- config.py
+++ config.py
@@ -15,14 +15,25 @@ if (based_input == '1'):
     recommended_margin = 5
 
     print("Here are the supported Pairs : ")
-    print("1. DOGE 🚀")
-    print("2. 1INCH")
+    print("1. ETH 🔥")
+    print("2. BNB")
+    print("3. DOGE 🚀")
+    print("4. LINK")
+    print("5. 1INCH 🦄")
+    print("6. SUSHI 🍣")
+    print("7. XRP")
     print("0. Others (Required more input)")
     asset_input = input("\nChoose your Pair : ")
 
     if asset_input == '0': asset = input("Enter your COIN SYMBOL (Ex: ETH): ").upper()
-    elif (asset_input == '2') or (asset_input == '1INCH'): asset = "1INCH"
-    else: asset = "DOGE"
+    elif (asset_input == '1') or (asset_input == 'ETH'): asset = "ETH"
+    elif (asset_input == '2') or (asset_input == 'BNB'): asset = "BNB"
+    elif (asset_input == '3') or (asset_input == 'DOGE'): asset = "DOGE"
+    elif (asset_input == '4') or (asset_input == 'LINK'): asset = "LINK"
+    elif (asset_input == '5') or (asset_input == '1INCH'): asset = "1INCH"
+    elif (asset_input == '6') or (asset_input == 'SUSHI'): asset = "SUSHI"
+    elif (asset_input == '7') or (asset_input == 'XRP'): asset = "XRP"
+    else: asset = "ETH"
 
     pair = asset + base
     core_input = input("Set BTC Amount for " + asset + "-" + base + " (Default " + str(recommended_core) + ") : ").upper() or recommended_core
