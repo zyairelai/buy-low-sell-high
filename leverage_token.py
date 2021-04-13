ba7cb5941d83f068e0d7499348c71c0bd1c4ced9
Zyaire <zyairelai@gmail.com>
2021-04-13 10:41:20 +0800

    add leverage token

---
+++ leverage_token.py
@@ -0,0 +1,92 @@
+live_trade = True
+asset = ["BNBUP", "BTCUP"]
+base  = ["USDT", "USDT"]
+core  = 300
+margin_percentage = 4
+pair,round_off = [], []
+
+for coin in base:
+    if coin == "USDT": decimal = 2
+    elif coin == "BTC": decimal = 6
+    elif coin == "ETH": decimal = 5
+    elif coin == "BNB": decimal = 3
+    else: decimal == 4
+    round_off.append(decimal)
+
+for i in range(len(asset)):
+    pair.append(asset[i] + base[i])
+
+try:
+    import os
+    import socket
+    import requests
+    import urllib3
+    from datetime import datetime
+    from termcolor import colored
+    from binance.client import Client
+    from binance.exceptions import BinanceAPIException
+    from apscheduler.schedulers.blocking import BlockingScheduler
+
+    # Get environment variables
+    api_key     = os.environ.get('API_KEY')
+    api_secret  = os.environ.get('API_SECRET')
+    client      = Client(api_key, api_secret)
+    
+    def buy_low_sell_high():
+        for i in range(len(pair)):
+            asset_info      = client.get_symbol_ticker(symbol=pair[i])
+            asset_price     = float(asset_info.get("price"))
+            asset_balance   = float(client.get_asset_balance(asset=asset[i]).get("free"))
+
+            current_core    = round(asset_balance * asset_price, round_off[i])
+            change_percent  = round(((current_core - core) / core * 100), 4)
+            trade_amount    = round(abs(current_core - core), round_off[i])
+
+            if (current_core > core) and (abs(change_percent) > margin_percentage):
+                if live_trade: client.order_market_sell(symbol=pair[i], quoteOrderQty=trade_amount)
+                print(colored(asset_info, "green"))
+                print(colored("Created at           : " + str(datetime.today().strftime("%d-%m-%Y @ %H:%M:%S")), "green"))
+                print(colored("Prefix Core          : " + str(core) + " " + base[i], "green"))
+                print(colored("Current Core         : " + str(current_core) + " " + base[i], "green"))
+                print(colored("Percentage Changed   : " + str(change_percent) + " %", "green"))
+                print(colored("Action               : SELL " + str(trade_amount) + " " + base[i] + "\n", "green"))
+
+            elif (current_core < core) and (abs(change_percent) > margin_percentage):
+                if live_trade: client.order_market_buy(symbol=pair[i], quoteOrderQty=trade_amount)
+                print(colored(asset_info, "red"))
+                print(colored("Created at           : " + str(datetime.today().strftime("%d-%m-%Y @ %H:%M:%S")), "red"))
+                print(colored("Prefix Core          : " + str(core) + " " + base[i], "red"))
+                print(colored("Current Core         : " + str(current_core) + " " + base[i], "red"))
+                print(colored("Percentage Changed   : " + str(change_percent) + " %", "red"))
+                print(colored("Action               : BUY " + str(trade_amount) + " " + base[i] + "\n", "red"))
+
+            else:
+                print(asset_info)
+                print("Created at           : " + str(datetime.today().strftime("%d-%m-%Y @ %H:%M:%S")))
+                print("Prefix Core          : " + str(core) + " " + base[i])
+                print("Current Core         : " + str(current_core) + " " + base[i])
+                print("Percentage Changed   : " + str(change_percent) + " %")
+                print("Action               : Do Nothing\n")
+
+    try:
+        scheduler = BlockingScheduler()
+        if live_trade: scheduler.add_job(buy_low_sell_high, 'cron', minute='0')
+        else: scheduler.add_job(buy_low_sell_high, 'interval', seconds=3)
+        scheduler.start()
+    except (KeyError,
+            socket.timeout,
+            BinanceAPIException,
+            ConnectionResetError,
+            urllib3.exceptions.ProtocolError,
+            urllib3.exceptions.ReadTimeoutError,
+            requests.exceptions.ConnectionError,
+            requests.exceptions.ConnectTimeout,
+            requests.exceptions.ReadTimeout) as e:
+
+        print(e)
+        # if not os.path.exists("Error_Message"): os.makedirs("Error_Message")
+        # with open((os.path.join("Error_Message", config.pair + ".txt")), "a") as error_message:
+        #     error_message.write("[!] " + config.pair + " - " + "Created at : " + datetime.today().strftime("%d-%m-%Y @ %H:%M:%S") + "\n")
+        #     error_message.write(str(e) + "\n\n")
+
+except KeyboardInterrupt: print("\n\nAborted.\n")
