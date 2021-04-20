d6247e2c40782557931a800463c50b49d6ed4f18
Zyaire <zyairelai@gmail.com>
2021-04-20 11:18:46 +0800

    heroku

--- buy_low_sell_high.py
+++ buy_low_sell_high.py
@@ -1,7 +1,7 @@
 live_trade = True
-asset = ["ADA", "BNB", "DOGE", "FET", "MATIC", "LINK", "LUNA", "SXP", "1INCH", "UNI", "TRX", "XRP"]
-base  = ["BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC"]
-core  = [0.005, 0.01, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005]
+asset = ["ADA", "BNB", "DOGE", "FET", "MATIC", "LINK", "LUNA", "SXP", "1INCH", "UNI", "XRP"]
+base  = ["BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC", "BTC"]
+core  = [0.005, 0.01, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005]
 margin_percentage = 4
 pair,round_off = [], []
 
